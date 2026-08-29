0abe59b4d55d1be7c5dbc6dcdfe70164933e55a0
Md Shalik <mdshalik486@gmail.com>
2026-08-29 23:41:14 +0530

    Day 03: Functions and Patterns practice

---
+++ Function-Question's/JavaBasics.java
@@ -0,0 +1,11 @@
+public class JavaBasics {
+    public static void printHelloWorld(){
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+    }
+    public static void main(String[] args) {
+        printHelloWorld();
+        
+    }
+}
