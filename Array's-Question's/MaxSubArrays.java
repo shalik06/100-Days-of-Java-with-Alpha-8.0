755bfafd4f126d4b13567bb00dd0494a5bafb7f0
Md Shalik <mdshalik486@gmail.com>
2026-09-05 22:17:04 +0530

    Array's Prolem

---
+++ Array's-Question's/MaxSubArrays.java
@@ -0,0 +1,37 @@
+public class MaxSubArrays {
+    public static void maxSubArraySum(int num[]) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+
+        for (int i = 0; i < num.length; i++) {
+            int start = i;
+
+            for (int j = i; j < num.length; j++) {
+                int end = j;
+                currSum =0;
+                for (int k = start; k <= end; k++) {
+                    currSum +=num[k];
+                }
+                System.out.println(currSum);
+                if (maxSum < currSum) {
+                    maxSum = currSum;
+                }
+                
+                 
+            }
+       
+
+        
+        }
+        System.out.println("max sum = " + maxSum);
+       
+         
+    }
+
+    public static void main(String[] args) {
+        int num[] = {1, -2, 6, -1, 3};
+        maxSubArraySum(num);
+    }
+}
+// Brute force;; solution...
