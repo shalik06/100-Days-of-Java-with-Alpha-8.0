8b86c8f85cd2cbeeace599b5ae1c5fe9da4c2d08
Md Shalik <mdshalik486@gmail.com>
2026-09-14 23:03:50 +0530

    Recursion-Question's

---
+++ PrintFirstNNatural.java
@@ -0,0 +1,14 @@
+public class PrintFirstNNatural {
+    public static int sum(int n){
+        if (n == 1) {
+            return 1;
+        }
+        int sum1 = sum(n-1);
+        int sn = n+sum1;
+        return sn;
+    }
+    public static void main(String[] args) {
+        int n =5;
+        System.out.println(sum(n));
+    }
+}
