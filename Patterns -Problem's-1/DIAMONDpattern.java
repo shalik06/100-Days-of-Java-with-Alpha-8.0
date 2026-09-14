dc8db27156de919cef68ecaeedc738f05c53e31c
Md Shalik <mdshalik486@gmail.com>
2026-09-14 23:13:25 +0530

    Add recursion questions and pattern updates

---
+++ Patterns -Problem's-1/DIAMONDpattern.java
@@ -0,0 +1,34 @@
+public class DIAMONDpattern {
+    public static void main(String[] args) {
+      
+        int n = 4;
+
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // Lower half
+        for (int i = n - 1; i >= 1; i--) {
+
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
