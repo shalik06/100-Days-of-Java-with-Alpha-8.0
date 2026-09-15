70ffdec1cc38272295352ca6e7ad16de5900e0ec
Md Shalik <mdshalik486@gmail.com>
2026-09-15 17:12:16 +0530

    Recursion Problems

---
+++ PrintNthFibonacciNum.java
@@ -0,0 +1,16 @@
+public class PrintNthFibonacciNum {
+    public static int fibonacci(int n){
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        int fnm1 = fibonacci(n-1);
+        int fnm2 = fibonacci(n-2);
+        int fn = fnm1+ fnm2;
+        return fn;
+
+    }
+    public static void main(String[] args) {
+        int n =26;
+        System.out.println(fibonacci(n));
+    }
+}
